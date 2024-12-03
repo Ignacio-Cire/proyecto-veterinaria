2f6e95fd58dfefe52c8f6105ac8b627bc9ba6f0c
Gabriel Ignacio Cire <gabriel.cire@est.fi.uncoma.edu.ar>
2024-12-03 16:32:18 -0300

    cambiando-estructura

--- login/views/Login/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión
-header('Location: ../home/index.php'); // Redirige al login
--- login/views/action/actionMenu.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "../../configuracion.php";
-include_once "../../Control/abmMenu.php";
-
-$obj = new abmMenu();
-echo json_encode($obj->armarMenu());
-?>
